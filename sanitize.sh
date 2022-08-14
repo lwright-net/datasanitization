96b0115f91d714c873521b4cbb76954cd1571913
Lane Wright <apen9uin@gmail.com>
2022-08-14 13:04:25 -0400

    added fundamental functionality of writing zeros

--- sanitize.sh
+++ sanitize.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-printf "This script is potentially dangerous! \n It WILL destroy data and make said data unrecoverable! \n \n"
+printf "---------------"
+printf "This script is potentially dangerous! \nIt WILL destroy data and make said data unrecoverable! \n \n"
 printf "Please choose disk to sanitize.\nBe mindful of which disk the OS is written to! \n"
+printf "---------------"
 lsblk
-read -p "Enter the disk name as shown in the chart above: " disktokill
-printf "You entered $disktokill. Is this the correct disk? \n:"
+read -p "Enter the disk name *EXACTLY* as shown in the chart above: " disktokill
+read -p "You entered $disktokill. Is this the correct disk? (y/n)\n" yn
+
+case $yn in
+    [Yy]* ) sudo dd if=/dev/zero of=/dev/$disktokill bs=64K status=progress ;
+    [Nn]* ) printf "Aborting sanitization!\n"
