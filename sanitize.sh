542783f68f9bc879180ca617c08d61b2df1cd4ad
Lane Wright <apen9uin@gmail.com>
2022-08-14 15:20:15 -0400

    edits to make it work right

--- sanitize.sh
+++ sanitize.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 printf "===============\n"
-printf "This script is potentially dangerous! \nIt WILL destroy data and make said data unrecoverable! \n \n"
+printf "This script is potentially dangerous! \nIt WILL destroy data and make said data unrecoverable! \n"
 printf "===============\n"
+finddisktokill() {
 printf "Please choose disk to sanitize.\nBe mindful of which disk the OS is written to! \n"
 lsblk
 read -p "Enter the disk name *EXACTLY* as shown in the chart above: " disktokill
+}
+finddisktokill()
 
 while true; do
 read -p "You selected $disktokill. Is this correct? (y/n)" yn
-    if [-f /dev/$disktokill]; then
+    if [-f "/dev/$disktokill"]; then
      case $yn in
          [Yy]* ) sudo dd if=/dev/zero of=/dev/$disktokill bs=64K status=progress ;
                 break;;
@@ -18,5 +21,6 @@ read -p "You selected $disktokill. Is this correct? (y/n)" yn
      esac
     else
      printf "$disktokill does not exist in /dev/ \n"
+     break;;
     fi
 done
