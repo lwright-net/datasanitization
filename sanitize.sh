bd223997eacb9c8640e72024a0719873c3bb9b12
Lane Wright <apen9uin@gmail.com>
2022-08-15 17:56:20 -0400

    added a double check

--- sanitize.sh
+++ sanitize.sh
@@ -11,16 +11,22 @@ finddisktokill()
 
 while true; do
 read -p "You selected $disktokill. Is this correct? (y/n)" yn
-    if [-f "/dev/$disktokill"]; then
+    if [ -b "/dev/$disktokill" ]; then
      case $yn in
-         [Yy]* ) sudo dd if=/dev/zero of=/dev/$disktokill bs=64K status=progress ;
-                break;;
-         [Nn]* ) printf "Aborting sanitization!\n" ;
+         [Yy]* ) read -p "Are you sure $disktokill is correct? (y/n)" yn2
+                 case $yn2 in
+                     [Yy]* ) sudo dd if=/dev/zero of=/dev/$disktokill bs=64K status=progress ;
+                             break;;
+                     * ) printf "Aborting sanitization!\n";
+                          break;;
+                 esac
                  break;;
-         * ) printf "Please answer yes or no." ;
+         * ) printf "Aborting sanitization!\n";
+              break;;
      esac
     else
      printf "$disktokill does not exist in /dev/ \n"
+     finddisktokill
     fi
 break;;
 done
