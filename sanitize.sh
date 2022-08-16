fefbca3b78a2ac2b97c9a51adfb299f1d8282b02
Lane Wright <apen9uin@gmail.com>
2022-08-16 19:08:15 -0400

    added pdf output and verification

--- sanitize.sh
+++ sanitize.sh
@@ -13,14 +13,29 @@ printf "\nWriting random data\n"
 sudo dd if=/dev/urandom of=/dev/$1 bs=64K status=progress
 }
 
+verify() {
+sudo hexdump /dev/$1 -Cs $((RANDOM%5)) -n 128
+}
+
+log() {
+    cat \
+    <(date) \
+    <(printf "The following disk has been sanitized\nIncluded is pertinent information\nand a random selection of bytes read from the disk\nafter sanitization\n") \
+    <(lsblk --nodeps -o name,model,serial,uuid,size /dev/$1) \
+    <(verify) > latestsanitization.log
+}
+logtopdf() {
+cupsfilter latestsanitization.log > $(date +"%b%d%Y%H%M").pdf
+}
+
 #!/bin/bash
 printf "===============\n"
 printf "This script is potentially dangerous! \nIt WILL destroy data and make said data unrecoverable! \n"
 printf "===============\n"
 finddisktokill() {
 printf "Please choose disk to sanitize.\nBe mindful of which disk the OS is written to! \n\n"
-lsblk
-read -p "\nEnter the disk name *EXACTLY* as shown in the chart above: " disktokill
+lsblk -o name,size,type,mountpoint,serial
+read -p "\n Enter the disk name *EXACTLY* as shown in the chart above: " disktokill
 }
 
 finddisktokill
@@ -34,6 +49,8 @@ while true; do
                      [Yy]* ) writezeros $disktokill;
                              writeones $disktokill;
                              writerandom $disktokill;
+                             log;
+                             logtopdf;
                              printf "\nSanitization completed!\n"
                              break;;
                      * ) printf "\nAborting sanitization!\n";
