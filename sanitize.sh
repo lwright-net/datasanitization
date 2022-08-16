bbe47a5b9b7e5b176dfac7eec5bdd9172ed19d24
Lane Wright <apen9uin@gmail.com>
2022-08-15 20:32:56 -0400

    defined writing functions and implemented them

--- sanitize.sh
+++ sanitize.sh
@@ -1,3 +1,18 @@
+writezeros() {
+printf "Writing zeros\n"
+sudo dd if=/dev/zero of=/dev/$1 bs=64K status=progress
+}
+
+writeones() {
+printf "Writing ones\n"
+tr '\0' '\377' < /dev/zero | sudo dd of=/dev/$1 bs=64K status=progress
+}
+
+writerandom() {
+printf "Writing random data\n"
+sudo dd if=/dev/urandom of=/dev/$1 bs=64K status=progress
+}
+
 #!/bin/bash
 printf "===============\n"
 printf "This script is potentially dangerous! \nIt WILL destroy data and make said data unrecoverable! \n"
@@ -7,15 +22,19 @@ printf "Please choose disk to sanitize.\nBe mindful of which disk the OS is writ
 lsblk
 read -p "Enter the disk name *EXACTLY* as shown in the chart above: " disktokill
 }
+
 finddisktokill
 
 while true; do
-read -p "You selected $disktokill. Is this correct? (y/n)" yn
     if [ -b "/dev/$disktokill" ]; then
+     read -p "You selected $disktokill. Is this correct? (y/n)" yn
      case $yn in
          [Yy]* ) read -p "Are you sure $disktokill is correct? (y/n)" yn2
                  case $yn2 in
-                     [Yy]* ) sudo dd if=/dev/zero of=/dev/$disktokill bs=64K status=progress ;
+                     [Yy]* ) writezeros $disktokill;
+                             writeones $disktokill;
+                             writerandom $disktokill;
+                             printf "Sanitization completed!\n"
                              break;;
                      * ) printf "Aborting sanitization!\n";
                           break;;
